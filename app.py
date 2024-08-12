583cf6a7e823b0ae4743ef83be1b610250d1fc3c
Mosawer Mahboob <mosawer.mahboob@roshan.af>
2024-08-12 16:00:06 +0430

    Dynamic dataset size and popover to show the raw dataset in a table overlayed on a sidebar.

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ class DatasetURL:
     dataset = "h9gi-nx95"
 
 @st.cache_data
-def load_data(url_object: DatasetURL, nrow: int = 2_000) -> pd.DataFrame:
+def load_data(url_object: DatasetURL, nrow: int) -> pd.DataFrame:
+    """
+
+    :param url_object: @dataclass object with two values: domain and dataset
+    :param nrow: an integer that specifies the number of rows to fetch
+    :return: pd.DataFrame
+    """
     client = Socrata(url_object.domain, None)
     results = client.get(url_object.dataset, limit=nrow)
 
@@ -20,11 +26,27 @@ def load_data(url_object: DatasetURL, nrow: int = 2_000) -> pd.DataFrame:
     results_df.dropna(subset=['latitude', 'longitude'], inplace=True)
 
     return results_df
-#%%
 
-data = load_data(DatasetURL(), 100_000)
-print(data.head())
+data = None
+#%%
+st.sidebar.header("Adjust dataset size")
+with st.sidebar:
+    user_nrow = 2_000
+    with st.form(key="submit"):
+        user_nrow = st.number_input("Enter dataset size", min_value=2_000, max_value=2_000_000)
+        submit_button = st.form_submit_button("Set")
+
+    data = load_data(DatasetURL(), user_nrow)
+
+    with st.popover("Show Raw Data"):
+        st.subheader("Raw Data")
+        st.write(data)
+
+
+if submit_button:
+    st.toast(f"Size set to: {user_nrow}")
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a Streamlit dashboard that can be used to analyze motor vehicle collisions in New York City.")
+
 
